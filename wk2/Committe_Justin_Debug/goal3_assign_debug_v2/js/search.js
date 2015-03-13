71064b545f0866983c5ed36ad1971b9f2bb7dc0a
jocommitte <jocommitte.com>
2015-03-13 19:44:45 -0400

    week2

--- wk2/Committe_Justin_Debug/goal3_assign_debug_v2/js/search.js
+++ wk2/Committe_Justin_Debug/goal3_assign_debug_v2/js/search.js
@@ -15,72 +15,67 @@ March 8, 2015
 	;
 	
 	// Validates search query
-	var validqte = function(query){//assignment operator needed
+	var validqte == function(query){//assignment operator needed
 		
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) == " "){//need another equals sign
+		while(query.charAt(0) = " "){//messed up method query??
 			query = query.substring(1, query.length);
 		};
-		while(query.charAt(query.length-1) === "") {
-            query = query.substring(0, query.length - 1);
-        };
+		while(query.charAt(query.length-1) === ""){
+			query = query.substring(0, query.length-1);
+		;
 		
 		// Check search length, must have 3 characters
 		if(query.length < 3){//it's good than or equal to
             //missing the second quotes
-			alert("Your search query is too small, try again.");
+			alert("Your search query is too small, try again.);
 			
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
 			searchInput.focus();
 			return;
 		};
-		 console.log("valid function")
+		
 		search(query);
 	};
 	
 	// Finds search matches
-	var search = function(query){  //missing semicolon and open bracket
+	var search = function(query)  //missing semicolon and open bracket
+		
 		// split the user's search query string into an array
-		var queryArray = query.split(" ");
-
-        // array to store matched results from database.js
+		var queryArray = query.join(" ");
+		
+		// array to store matched results from database.js
 		var results = [];
 
 		// loop through each index of db array
-		for(var i=0, j=db.length; i<j; i++) {
-
-            // each db[i] is a single video item, each title ends with a pipe "|"
-            // save a lowercase variable of the video title
-            var dbTitleEnd = db[i].indexOf('|');
-            var dbItem = db[i].toLowerCase().substring(0, dbTitleEnd);
-
-            console.log('start of search')
-
-            // loop through the user's search query words
-            // save a lowercase variable of the search keyword
-            for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
-                var qItem = queryArray[ii].toLowerCase();
-
-                // is the keyword anywhere in the video title?
-                // If a match is found, push full db[i] into results array
-                var compare = dbItem.indexOf(qItem);
-                if (compare !== -1) {
-                    results.push(db[i]);
-                };
-            };//missing bracket
-            console.log(results);
-        };//missing bracket
-
-        results.sort();
-
-        // Check that matches were found, and run output functions
-		if(results.length == 0){
+		for(var i=0, j=db.length; i<j; i++){
+		
+			// each db[i] is a single video item, each title ends with a pipe "|"
+			// save a lowercase variable of the video title
+			var dbTitleEnd = db[i].indexOf('|');
+			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+			
+			// loop through the user's search query words
+			// save a lowercase variable of the search keyword
+			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
+				var qitem = queryArray[ii].tolowercase();
+				
+				// is the keyword anywhere in the video title?
+				// If a match is found, push full db[i] into results array
+				var compare = dbitem.indexOf(qitem);
+				if(compare !== -1){
+					results.push(db[i]);
+				};
+			;//missing bracket
+		;//missing bracket
+		
+		results.sort();
+		
+		// Check that matches were found, and run output functions
+		if(results.length = 0){
 			noMatch();
-            console.log("if");
-
-        }else{
+		}else{
 			showMatches(results);
-
 		};
 	};
 	
@@ -121,13 +116,13 @@ March 8, 2015
 	
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
-	document.forms[0].onsubmit = function() {//missing camelCase
-        var query = searchInput.value;
-        validqte(query);
+	document.forms[0].onsubmit = function(){//missing camelCase
+		var query = searchInput.value;
+		validqte(query);
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
-        return false;
-    };
+		return false;
+	;
 
 })();
